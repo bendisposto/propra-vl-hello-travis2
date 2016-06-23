59b650b6f69aac4270dfb6d9ad61fe99a9c45d19
Jens Bendisposto <jens@bendisposto.de>
2016-06-23 13:18:22 +0200

    initial version

---
+++ src/main/java/CliMain.java
@@ -0,0 +1,42 @@
+
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+
+public class CliMain {
+
+	public static void main(String[] args) {
+
+		Option a = new Option("file", true, "The filename");
+		a.setArgs(1);
+		a.setArgName("FILENAME");
+		a.setType(String.class);
+		a.setRequired(true);
+
+		Option b = new Option("foo", "Do you want to foo (optional)");
+
+		Options opts = new Options()
+				.addOption(a)
+				.addOption(b);
+
+		CommandLineParser parser = new DefaultParser();
+
+		try {
+			CommandLine commandLine = parser.parse(opts, args);
+			boolean foo = commandLine.hasOption("foo");
+			String filename = commandLine.getOptionValue("file");
+			System.out.println("File: "+filename + " Foo: "+foo);
+		} catch (ParseException e) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("java CliMain", opts);
+		}
+		
+	}
+
+}
